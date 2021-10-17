3bde1d4cfb3389a6788c420868350b713e15de92
Val Lorentz <progval+git@progval.net>
2021-10-17 14:06:31 +0200

    Add copyright headers
    It was already de-facto AGPLv3 given the LICENSE at the root.

--- test/matrix_client/client_test.exs
+++ test/matrix_client/client_test.exs
@@ -1,3 +1,19 @@
+##
+# Copyright (C) 2021  Valentin Lorentz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###
+
 defmodule Matrix2051.MatrixClient.ClientTest do
   use ExUnit.Case
   doctest Matrix2051.MatrixClient.Client
