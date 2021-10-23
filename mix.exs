7fa2b4bf10a91c838b33a2fbb30a62ff534b4fcc
Val Lorentz <progval+git@progval.net>
2021-10-23 19:09:37 +0200

    Clean up auto-generated files

--- mix.exs
+++ mix.exs
@@ -27,14 +27,12 @@ defmodule Matrix2051.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # only using :mochiweb_html
