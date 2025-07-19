340cdc006295d50a90d1afbc1e6fb7dc92fa973f
Val Lorentz <progval+git@progval.net>
2025-07-19 16:25:14 +0200

    Fix regex

--- lib/irc/handler.ex
+++ lib/irc/handler.ex
@@ -446,7 +446,7 @@ defmodule M51.IrcConn.Handler do
                         # this was only meant to be used with Pantalaimon
                         proxy =
                           case Regex.named_captures(
-                                 ~R((plaintextproxy|homeserver-url)=(?<url>https?://\S*\)),
+                                 ~R((plaintextproxy|homeserver-url\)=(?<url>https?://\S*\)),
                                  M51.IrcConn.State.gecos(state)  || ""
                                ) do
                             nil -> nil
